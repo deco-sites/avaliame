cc81c36d70d28c6fbb7101b650f67f19cc13f437
viniciusventura29 <viniciusventura29@gmail.com>
2024-07-13 19:32:23 -0300

    improve button style

--- components/product/ProductInfo.tsx
+++ components/product/ProductInfo.tsx
@@ -35,9 +35,10 @@ function ProductInfo({ page }: Props) {
 
   const { price = 0, listPrice, seller = "1", availability } = useOffer(offers);
 
-  const percent = listPrice && price
-    ? Math.round(((listPrice - price) / listPrice) * 100)
-    : 0;
+  const percent =
+    listPrice && price
+      ? Math.round(((listPrice - price) / listPrice) * 100)
+      : 0;
 
   const breadcrumb = {
     ...breadcrumbList,
@@ -65,11 +66,12 @@ function ProductInfo({ page }: Props) {
   });
 
   //Checks if the variant name is "title"/"default title" and if so, the SKU Selector div doesn't render
-  const hasValidVariants = isVariantOf?.hasVariant?.some(
-    (variant) =>
-      variant?.name?.toLowerCase() !== "title" &&
-      variant?.name?.toLowerCase() !== "default title",
-  ) ?? false;
+  const hasValidVariants =
+    isVariantOf?.hasVariant?.some(
+      (variant) =>
+        variant?.name?.toLowerCase() !== "title" &&
+        variant?.name?.toLowerCase() !== "default title"
+    ) ?? false;
 
   return (
     <div {...viewItemEvent} class="flex flex-col" id={id}>
@@ -78,7 +80,7 @@ function ProductInfo({ page }: Props) {
         class={clx(
           "text-sm/4 font-normal text-black bg-primary bg-opacity-15 text-center rounded-badge px-2 py-1",
           percent < 1 && "opacity-0",
-          "w-fit",
+          "w-fit"
         )}
       >
         {percent} % off
@@ -106,25 +108,25 @@ function ProductInfo({ page }: Props) {
 
       {/* Add to Cart and Favorites button */}
       <div class="mt-4 sm:mt-10 flex flex-col gap-2">
-        {availability === "https://schema.org/InStock"
-          ? (
-            <>
-              <AddToCartButton
-                item={item}
-                seller={seller}
-                product={product}
-                class="btn btn-primary no-animation"
-                disabled={false}
-              />
-              <WishlistButton item={item} />
-              <a href={`/feedback/${productId}`}>
-                <button class="btn btn-primary no-animation">
-                  Avaliar AAAAAAA
-                </button>
-              </a>
-            </>
-          )
-          : <OutOfStock productID={productID} />}
+        {availability === "https://schema.org/InStock" ? (
+          <>
+            <AddToCartButton
+              item={item}
+              seller={seller}
+              product={product}
+              class="btn btn-primary no-animation"
+              disabled={false}
+            />
+            <WishlistButton item={item} />
+            <a class="mt-4 w-full" href={`/feedback/${productId}`}>
+              <button class="btn no-animation w-full">
+                Escreva uma avaliação
+              </button>
+            </a>
+          </>
+        ) : (
+          <OutOfStock productID={productID} />
+        )}
       </div>
 
       {/* Shipping Simulation */}
